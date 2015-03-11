7ffad72edd8c45962669006eb00c229e93920a8f
Daniel Hammarström <d.hammarstrom@hotmail.se>
2015-03-11 20:07:27 +0100

    updated functions

--- R/riseBaseline.R
+++ R/riseBaseline.R
@@ -4,9 +4,10 @@
 #' @param data A data frame with work intensity (e.g. watt) and lactate measurements
 #' @param workload.column An integer specifying the column with work intensity data, default=1. 
 #' @param lactate.column An integer specifying the column with lactate data, default=2.
+#' @param rise.from.baseline A numeric specifying the rise frome baseline, default=1
+#' @param estimate A logical specifying if baseline should be estimated from the model fit or be defined by the lowest measured value, default=TRUE
 #' @param plot Logical specify whether to add plot or not. The plot shows data points with the model fit
-#' @param lactate A number specifying the lacate level at which you want to now the work intensity.
-#' @return Work intensity at a specified lactate concentration 
+#' @return Work intensity at a specified rise from baseline lactate concentration 
 #' @import dplyr
 #' @import tidyr
 #' @import ggplot2
@@ -15,17 +16,32 @@
 #' John Newell , David Higgins , Niall Madden , James Cruickshank , Jochen Einbeck , Kenny McMillan , Roddy McDonald. 
 #' Journal of Sports Sciences, Vol. 25, Iss. 12, 2007
 
-riseBaseline<-function(data, workload.column=1, lactate.column=2, plot=T, rise=1){
+riseBaseline<-function(data, workload.column=1, lactate.column=2, rise.from.baseline=1, estimate=T, plot=T){
       
       data<-data.frame(data)
       
       
       model <-lm(data[,lactate.column]~data[,workload.column]+I(data[,workload.column]^2)+I(data[,workload.column]^3))
       poly.coefs <- model$coefficients
+    
+      if(estimate==TRUE){
+            
+            workload.grid<-seq(from=data[which.min(data[,workload.column]),workload.column], to=data[which.max(data[,workload.column]),workload.column], length.out = 1000)
+            
+            lactate.grid<-poly.coefs[1]+poly.coefs[2]*workload.grid+poly.coefs[3]*workload.grid^2+poly.coefs[4]*workload.grid^3
       
-      fixed.lactate<- Re(polyroot(c(poly.coefs[1]-lactate,poly.coefs[2],poly.coefs[3],poly.coefs[4])))
+            tmp<-data.frame(workload.grid, lactate.grid)
       
-      fixed.lactate<-max(fixed.lactate[fixed.lactate<=max(data[,workload.column])])
+            target.lactate<-tmp[which.min(tmp[,2]),2]+rise.from.baseline
+      
+      } 
+      
+      if(estimate==FALSE){
+            target.lactate<-min(data[,lactate.column], na.rm=T)+rise.from.baseline
+      }
+      
+      lactate.rise<- Re(polyroot(c(poly.coefs[1]-target.lactate,poly.coefs[2],poly.coefs[3],poly.coefs[4])))
+      lactate.rise<-max(lactate.rise[lactate.rise<=max(data[,workload.column])])
       
       if(plot==TRUE){
             
@@ -33,9 +49,9 @@ riseBaseline<-function(data, workload.column=1, lactate.column=2, plot=T, rise=1
             names(data)[lactate.column]<-"Lactate"
             
             segment<-data.frame(x=min(data[,workload.column], na.rm=T),
-                                xend=fixed.lactate,
+                                xend=lactate.rise,
                                 y=min(data[,lactate.column], na.rm=T),
-                                yend=lactate)
+                                yend=target.lactate)
             
             plot<-data%>%
                   filter(!is.na(Lactate))%>%
@@ -46,11 +62,11 @@ riseBaseline<-function(data, workload.column=1, lactate.column=2, plot=T, rise=1
                   geom_segment(data=segment, aes(x=x,xend=xend,y=yend,yend=yend))+
                   geom_segment(data=segment, aes(x=xend,xend=xend,y=y,yend=yend))
             
-            return(list(round(fixed.lactate,1), plot))
+            return(list(round(lactate.rise,1), plot))
             
       }
       
-      return(round(fixed.lactate,1))
+      return(round(lactate.rise,1))
       
       
 }
